527e6f77f25871af30c83f581c9a9d957334f616
rogulik <malvla2008@gmail.com>
2024-07-09 12:15:07 +0300

    Unit of work interfaces written

---
+++ EichkustMusic.Tracks.Application/UnitOfWork/Repositories/IPlaylistRepository.cs
@@ -0,0 +1,23 @@
+﻿using EichkustMusic.Tracks.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EichkustMusic.Tracks.Application.UnitOfWork.Repositories
+{
+    public interface IPlaylistRepository
+    {
+        Task<Playlist> GetByIdAsync(int id);
+
+        Task<IEnumerable<Playlist>> ListAsync(
+            int pageNum, int pageSize, string? search);
+
+        void Delete(Playlist playlist);
+
+        void Add(Playlist playlist);
+
+        void AddTrack(Playlist playlist, Track track);
+    }
+}
