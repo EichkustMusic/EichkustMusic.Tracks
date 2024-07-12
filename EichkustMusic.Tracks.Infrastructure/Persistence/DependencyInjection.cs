fe9263b5715f91db8515cff296574adb17bbc648
rogulik <malvla2008@gmail.com>
2024-07-12 10:28:02 +0300

    Npgsql connected

---
+++ EichkustMusic.Tracks.Infrastructure/Persistence/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EichkustMusic.Tracks.Infrastructure.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(
+            this IServiceCollection services, IConfigurationManager configurationManager)
+        {
+            return services.AddDbContext<TracksDbContext>(o =>
+                o.UseNpgsql(configurationManager.GetConnectionString("TracksDb")));
+        }
+    }
+}
