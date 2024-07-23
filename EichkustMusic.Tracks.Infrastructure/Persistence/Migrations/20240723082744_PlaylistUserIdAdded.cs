ccc665c80b415b45bbb13d61388eb8a707e514cf
rogulik <malvla2008@gmail.com>
2024-07-23 11:28:39 +0300

    UserId added to Playlist entity, DB migrated

---
+++ EichkustMusic.Tracks.Infrastructure/Persistence/Migrations/20240723082744_PlaylistUserIdAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EichkustMusic.Tracks.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class PlaylistUserIdAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Playlists",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Playlists");
+        }
+    }
+}
